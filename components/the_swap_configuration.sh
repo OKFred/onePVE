0344217698ad7dc018c1614c97dc1838ab079175
Zhang Qi <zhangqi@tr-ag.com>
2023-12-24 21:57:47 +0800

    hotfix-swap配置继续优化/1.0.4

--- components/the_swap_configuration.sh
+++ components/the_swap_configuration.sh
@@ -17,12 +17,14 @@ the_swap_configuration() {
     local replace_regex=\/dev
     local flags='g'
     local sed_command="'s/$search_regex/$replace_regex/$flags'"
+    echo $sed_command
     sed "$sed_command" $swap_config_file #启用SWAP
   else
     local search_regex=^\/dev
     local replace_regex=#\/dev
     local flags='g'
     local sed_command="'s/$search_regex/$replace_regex/$flags'"
+    echo $sed_command
     sed "$sed_command" $swap_config_file #禁用SWAP
   fi
 }
@@ -31,7 +33,7 @@ the_file_backup() {
   local target=$1
   local backup_file="$target.bak"
   if [ ! -f "$backup_file" ]; then
-    mv $target $backup_file
+    cp $target $backup_file
     echo "已备份文件$target为$backup_file"
   else
     echo "备份文件已存在，跳过"
